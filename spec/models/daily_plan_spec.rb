c3e62fcc066486e27e9fcc5dc28aee219add1f42
Marko Lovic <markolovic@hotmail.com>
2015-02-15 22:18:15 +0100

    Add plan validation spec

--- spec/models/daily_plan_spec.rb
+++ spec/models/daily_plan_spec.rb
@@ -1,15 +1,14 @@
 require 'rails_helper'
 
 describe DailyPlan do
-	context 'should' do
-		
-	end
+	let(:plan) { DailyPlan.create }
 	it 'should not be valid without item quantity' do
 		#TODO BUG: only validates items when updating, and not on create
-		plan = DailyPlan.create
 		plan.items << build(:item)
 		expect(plan).not_to be_valid
 	end
 	it 'should be valid with item quantity !nil' do
+		plan.items << build(:item, quantity: 1000)
+		expect(plan).to be_valid
 	end
 end
