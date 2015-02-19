0e45771a07ecab7816ddf04e7a258a75c690ba87
Marko Lovic <markolovic@hotmail.com>
2015-02-19 12:14:08 +0100

    Remove old controller files and update routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-	root 'daily_plan#show'
+	root 'daily_plans#show'
+  resources :daily_plans
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
